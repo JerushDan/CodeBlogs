a4a73a6861cb6ff050dbdfb3364247e1ad827a8c
JerushDan <167603765+JerushDan@users.noreply.github.com>
2025-11-19 13:33:47 +0530

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -9,7 +9,7 @@ export const adminLogin = async (req, res)=>{
         if(email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD){
             return res.json({success: false, message: "Invalid Credentials"})
         }
-
+  
         const token = jwt.sign({email}, process.env.JWT_SECRET)
         res.json({success: true, token})
     } catch (error) {
@@ -71,5 +71,6 @@ export const approveCommentById = async (req, res) =>{
     }
 
 }   
+
 
 
